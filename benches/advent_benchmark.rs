847e9ad56b2d38868740b07b54b39fcd40d37c85
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-12-04 11:35:36 -0500

    Day 4 slices instead of HashSet and Vec

--- benches/advent_benchmark.rs
+++ benches/advent_benchmark.rs
@@ -3,17 +3,25 @@ use advent_of_code_2023::util::{get_input, DaySolver};
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 
 pub fn day4_benchmark(c: &mut Criterion) {
-    let input = get_input(4);
-    let input2 = input.clone();
-    let input3 = input.clone();
+    let input1 = get_input(4);
+    let input2 = input1.clone();
+    let input3 = input1.clone();
+    let input4 = input1.clone();
+    c.bench_function("day 4 parse", |b| {
+        b.iter_batched(|| &input1, Day4::parse, BatchSize::SmallInput)
+    });
     c.bench_function("day 4 part 1", move |b| {
-        b.iter_batched(|| input.clone(), Day4::part1, BatchSize::SmallInput)
+        b.iter_batched(|| input2.clone(), Day4::part1, BatchSize::SmallInput)
     });
     c.bench_function("day 4 part 2", move |b| {
-        b.iter_batched(|| input2.clone(), Day4::part2, BatchSize::SmallInput)
+        b.iter_batched(|| input3.clone(), Day4::part2, BatchSize::SmallInput)
     });
-    c.bench_function("day 4 parse", move |b| {
-        b.iter_batched(|| input3.clone(), Day4::parse, BatchSize::SmallInput)
+    let cards = Day4::parse(&input4);
+    c.bench_function("day 4 part 1 no parse", |b| {
+        b.iter_batched(|| &cards, Day4::_part1, BatchSize::SmallInput)
+    });
+    c.bench_function("day 4 part 2 no parse", |b| {
+        b.iter_batched(|| &cards, Day4::_part2, BatchSize::SmallInput)
     });
 }
 
--- src/day4.rs
+++ src/day4.rs
@@ -9,19 +9,16 @@ type Solution = usize;
 
 pub struct Day4();
 
-#[derive(Debug)]
 pub struct Card {
     number: u32,
-    winners: HashSet<u32>,
-    have: Vec<u32>,
+    winners: [u32; 10],
+    have: [u32; 25],
 }
 
 impl From<&String> for Card {
     fn from(value: &String) -> Self {
-        //
-        // Card   1: 84 17 45 77 11 66 94 28 71 70 | 45 51 86 83 53 58 64 30 67 96 41 89  8 17 33 50 80 84  6  2 87 72 27 63 77
         let number: u32 = value[5..8].trim().parse().unwrap();
-        let winners = HashSet::from([
+        let winners = [
             value[10..12].trim().parse().unwrap(),
             value[13..15].trim().parse().unwrap(),
             value[16..18].trim().parse().unwrap(),
@@ -32,8 +29,8 @@ impl From<&String> for Card {
             value[31..33].trim().parse().unwrap(),
             value[34..36].trim().parse().unwrap(),
             value[37..39].trim().parse().unwrap(),
-        ]);
-        let have = vec![
+        ];
+        let have = [
             value[42..44].trim().parse().unwrap(),
             value[45..47].trim().parse().unwrap(),
             value[48..50].trim().parse().unwrap(),
@@ -78,36 +75,40 @@ impl Card {
 }
 
 impl Day4 {
-    pub fn parse(input: Vec<String>) -> Vec<Card> {
+    pub fn parse(input: &[String]) -> Vec<Card> {
         input.iter().map(|line| line.into()).collect()
     }
-}
-
-impl DaySolver<Solution> for Day4 {
-    fn part1(input: Vec<String>) -> Option<Solution> {
-        let cards = Self::parse(input);
-        Some(
-            cards
-                .iter()
-                .map(|card| {
-                    let exp = card.matches() as u32;
-                    if exp == 0 {
-                        0
-                    } else {
-                        2_usize.pow(exp - 1)
-                    }
-                })
-                .sum(),
-        )
+    pub fn _part1(cards: &[Card]) -> Solution {
+        cards
+            .iter()
+            .map(|card| {
+                let exp = card.matches() as u32;
+                if exp == 0 {
+                    0
+                } else {
+                    2_usize.pow(exp - 1)
+                }
+            })
+            .sum()
     }
-    fn part2(input: Vec<String>) -> Option<Solution> {
-        let cards = Self::parse(input);
+    pub fn _part2(cards: &[Card]) -> Solution {
         let mut copies = vec![1; cards.len()];
         for i in 0..cards.len() {
             for j in 1..=cards[i].matches() {
                 copies[i + j] += copies[i];
             }
         }
-        Some(copies.iter().sum())
+        copies.iter().sum()
+    }
+}
+
+impl DaySolver<Solution> for Day4 {
+    fn part1(input: Vec<String>) -> Option<Solution> {
+        let cards = Self::parse(&input);
+        Some(Self::_part1(&cards))
+    }
+    fn part2(input: Vec<String>) -> Option<Solution> {
+        let cards = Self::parse(&input);
+        Some(Self::_part2(&cards))
     }
 }
