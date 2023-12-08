ac531fc19c988d74810aa0a58a31d084d41c03be
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-12-08 00:32:02 -0500

    Day 8 part 1

--- src/day8.rs
+++ src/day8.rs
@@ -3,24 +3,65 @@ use crate::util::DaySolver;
 
 type Solution = usize;
 
-pub struct Day8();
+pub type Directions = Vec<usize>;
 
-impl Day8 {
-    fn parse(input: Vec<String>) -> usize {
-        0
-    }
+#[derive(Debug)]
+pub struct Graph {
+    start: usize,
+    end: usize,
+    nodes: Vec<[usize; 2]>,
+}
 
-    fn solve(input: Vec<String>) -> Option<Solution> {
-        let data = Self::parse(input);
-        None
+#[derive(Debug)]
+pub struct Docs {
+    directions: Directions,
+    graph: Graph,
+}
+
+impl From<&[String]> for Docs {
+    fn from(value: &[String]) -> Self {
+        let directions = value[0]
+            .chars()
+            .map(|c| match c {
+                'L' => 0,
+                'R' => 1,
+                _ => panic!(),
+            })
+            .collect();
+        let node_names: Vec<&str> = value[2..].iter().map(|line| &line[0..3]).collect();
+        let start = node_names.iter().position(|x| x == &"AAA").unwrap();
+        let end = node_names.iter().position(|x| x == &"ZZZ").unwrap();
+        let nodes = value[2..]
+            .iter()
+            .map(|line| {
+                [
+                    node_names.iter().position(|x| x == &&line[7..10]).unwrap(),
+                    node_names.iter().position(|x| x == &&line[12..15]).unwrap(),
+                ]
+            })
+            .collect();
+        let graph = Graph { start, end, nodes };
+        Docs { directions, graph }
     }
 }
 
+pub struct Day8();
+
 impl DaySolver<Solution> for Day8 {
     fn part1(input: Vec<String>) -> Option<Solution> {
-        Self::solve(input)
+        let docs = Docs::from(&input[..]);
+        let mut node = docs.graph.start;
+        let mut steps = 0;
+        for dir in docs.directions.iter().cycle() {
+            if node == docs.graph.end {
+                break;
+            }
+            node = docs.graph.nodes[node][*dir];
+            steps += 1;
+        }
+        Some(steps)
     }
     fn part2(input: Vec<String>) -> Option<Solution> {
-        Self::solve(input)
+        None
     }
 }
