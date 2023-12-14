3721297cba3d14b2b54251d9c45389f89248a938
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-12-14 09:35:24 -0500

    Day 14 part 1

--- src/day14.rs
+++ src/day14.rs
@@ -1,26 +1,54 @@
 #![allow(unused)]
 use crate::util::DaySolver;
 
-type Solution = usize;
+type Rock = char;
+
+pub struct Platform {
+    rocks: Vec<Vec<Rock>>,
+}
+
+impl Platform {
+    pub fn roll_north(&mut self) {
+        for y in 0..self.rocks.len() {
+            for x in 0..self.rocks[0].len() {
+                if self.rocks[y][x] == 'O' {
+                    self.rocks[y][x] = '.';
+                    let mut yy = y;
+                    while yy > 0 && self.rocks[yy - 1][x] == '.' {
+                        yy -= 1;
+                    }
+                    self.rocks[yy][x] = 'O';
+                }
+            }
+        }
+    }
+    pub fn weight(&self) -> usize {
+        self.rocks
+            .iter()
+            .enumerate()
+            .map(|(y, row)| (self.rocks.len() - y) * row.iter().filter(|c| c == &&'O').count())
+            .sum()
+    }
+}
 
 pub struct Day14();
 
 impl Day14 {
-    fn parse(input: Vec<String>) -> usize {
-        0
-    }
-
-    fn solve(input: Vec<String>) -> Option<Solution> {
-        let data = Self::parse(input);
-        None
+    fn parse(input: &[String]) -> Platform {
+        Platform {
+            rocks: input.iter().map(|line| line.chars().collect()).collect(),
+        }
     }
 }
 
+type Solution = usize;
 impl DaySolver<Solution> for Day14 {
     fn part1(input: Vec<String>) -> Option<Solution> {
-        Self::solve(input)
+        let mut platform = Self::parse(&input);
+        platform.roll_north();
+        Some(platform.weight())
     }
     fn part2(input: Vec<String>) -> Option<Solution> {
-        Self::solve(input)
+        None
     }
 }
