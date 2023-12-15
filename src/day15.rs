21b7a6205c74b1009f6afb07b630fec557a455b4
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-12-15 09:52:30 -0500

    Day 15 superior hash storage

--- src/day15.rs
+++ src/day15.rs
@@ -7,7 +7,7 @@ pub struct Day15();
 
 #[allow(clippy::upper_case_acronyms)]
 pub struct HASHMAP {
-    boxes: [Vec<(String, usize)>; 256],
+    boxes: [Vec<(u64, u64)>; 256],
 }
 impl Default for HASHMAP {
     fn default() -> Self {
@@ -18,23 +18,25 @@ impl Default for HASHMAP {
 }
 impl HASHMAP {
     pub fn remove(&mut self, key: &str) {
+        let hash = Day15::big_hash(0, key);
         let index = Day15::hash(0, key);
         for i in 0..self.boxes[index].len() {
-            if self.boxes[index][i].0 == key {
+            if self.boxes[index][i].0 == hash {
                 self.boxes[index].remove(i);
                 return;
             }
         }
     }
-    pub fn insert(&mut self, key: &str, value: usize) {
+    pub fn insert(&mut self, key: &str, value: u64) {
+        let hash = Day15::big_hash(0, key);
         let index = Day15::hash(0, key);
         for i in 0..self.boxes[index].len() {
-            if self.boxes[index][i].0 == key {
+            if self.boxes[index][i].0 == hash {
                 self.boxes[index][i].1 = value;
                 return;
             }
         }
-        self.boxes[index].push((key.to_string(), value));
+        self.boxes[index].push((hash, value));
     }
     pub fn operate(&mut self, operation: &str) {
         if operation.ends_with('-') {
@@ -44,19 +46,19 @@ impl HASHMAP {
             self.insert(key, value.parse().unwrap());
         }
     }
-    pub fn focusing_power(&self) -> usize {
+    pub fn focusing_power(&self) -> u64 {
         self.boxes
             .iter()
             .enumerate()
             .map(|(i, boxxy)| {
-                (i + 1)
+                (i + 1) as u64
                     * boxxy
                         .iter()
                         .enumerate()
-                        .map(|(j, lens)| (j + 1) * lens.1)
-                        .sum::<usize>()
+                        .map(|(j, lens)| (j + 1) as u64 * lens.1)
+                        .sum::<u64>()
             })
-            .sum()
+            .sum::<u64>()
     }
 }
 
@@ -68,6 +70,16 @@ impl Day15 {
             .map(|c| *c as usize)
             .fold(0, |c, v| ((c + v) * 17) % 256)
     }
+    pub fn big_hash(current: u64, value: &str) -> u64 {
+        value
+            .as_bytes()
+            .iter()
+            .map(|c| *c as u64)
+            .enumerate()
+            .map(|(i, c)| (2 << i) * c)
+            .sum()
+        // .fold(0, |c, v| (c + v) * 17)
+    }
     pub fn part2_load(input: &[String]) -> HASHMAP {
         let mut hm = HASHMAP::default();
         input[0].split(',').for_each(|step| hm.operate(step));
@@ -75,10 +87,15 @@ impl Day15 {
     }
 }
 
-type Solution = usize;
+type Solution = u64;
 impl DaySolver<Solution> for Day15 {
     fn part1(input: Vec<String>) -> Option<Solution> {
-        Some(input[0].split(',').map(|step| Self::hash(0, step)).sum())
+        Some(
+            input[0]
+                .split(',')
+                .map(|step| Self::hash(0, step) as u64)
+                .sum(),
+        )
     }
     fn part2(input: Vec<String>) -> Option<Solution> {
         let mut hm = Self::part2_load(&input);
