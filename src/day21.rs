da969443c024766fd3f98e5e6f9005c890875a8f
Daniel Chiquito <daniel.chiquito@gmail.com>
2023-12-26 19:18:37 -0500

    Day 21 part 2 but it doesn't work

--- src/day21.rs
+++ src/day21.rs
@@ -3,11 +3,12 @@ use std::collections::{HashMap, HashSet};
 
 use crate::util::DaySolver;
 
-#[derive(Eq, PartialEq)]
+#[derive(Clone, Eq, PartialEq)]
 pub enum Tile {
     Rock,
     Plot(bool),
 }
+#[derive(Clone)]
 pub struct Field {
     tiles: HashMap<(i32, i32), Tile>,
     width: i32,
@@ -15,9 +16,19 @@ pub struct Field {
     start: (i32, i32),
 }
 impl Field {
+    pub fn visit(&mut self, x: i32, y: i32) -> bool {
+        let x_mod = ((x % self.width) + self.width) % self.width;
+        let y_mod = ((y % self.height) + self.height) % self.height;
+        if self.tiles.get(&(x_mod, y_mod)) == Some(&Tile::Rock) {
+            false
+        } else {
+            self.tiles.insert((x, y), Tile::Plot(true));
+            true
+        }
+    }
     pub fn print(&self, even: bool) {
-        for y in 0..self.height {
-            for x in 0..self.width {
+        for y in -self.height..self.height * 2 {
+            for x in -self.width..self.width * 2 {
                 match self.tiles.get(&(x, y)) {
                     Some(Tile::Rock) => print!("#"),
                     Some(Tile::Plot(false)) => print!("."),
@@ -28,11 +39,39 @@ impl Field {
                             print!(".");
                         }
                     }
-                    _ => {}
+                    _ => print!(" "),
                 }
             }
             println!();
         }
+    }
+    pub fn count(&self, start: (i32, i32), steps: i32, even: bool) -> u64 {
+        let mut field: Field = self.clone();
+        let mut to_visit = HashSet::from([start]);
+        for _ in 0..=steps {
+            let mut next_visits = HashSet::new();
+            for (x, y) in to_visit {
+                if field.visit(x, y) {
+                    next_visits.insert((x + 1, y));
+                    next_visits.insert((x - 1, y));
+                    next_visits.insert((x, y + 1));
+                    next_visits.insert((x, y - 1));
+                }
+            }
+            to_visit = next_visits;
+        }
+        // field.print(even);
+        field
+            .tiles
+            .iter()
+            .filter(|((x, y), tile)| {
+                if tile == &&Tile::Plot(true) {
+                    even == ((x + y) % 2 == 0)
+                } else {
+                    false
+                }
+            })
+            .count() as u64
     }
 }
 
@@ -73,46 +112,30 @@ impl Day21 {
     }
 }
 
-type Solution = usize;
+type Solution = u64;
 impl DaySolver<Solution> for Day21 {
     fn part1(input: Vec<String>) -> Option<Solution> {
         let mut field = Self::parse(&input);
-        field.print(true);
-        let mut to_visit = HashSet::new();
-        to_visit.insert(field.start);
-        for _ in 0..=64 {
-            let mut next_visits = HashSet::new();
-            for (x, y) in to_visit {
-                if let Some(&Tile::Plot(visited)) = field.tiles.get(&(x, y)) {
-                    if !visited {
-                        field.tiles.insert((x, y), Tile::Plot(true));
-                    }
-                    next_visits.insert((x + 1, y));
-                    next_visits.insert((x - 1, y));
-                    next_visits.insert((x, y + 1));
-                    next_visits.insert((x, y - 1));
-                }
-            }
-            to_visit = next_visits;
-        }
-        field.print(true);
-        Some(
-            field
-                .tiles
-                .iter()
-                .filter(|((x, y), tile)| {
-                    if tile == &&Tile::Plot(true) {
-                        (x + y) % 2 == 0
-                    } else {
-                        false
-                    }
-                })
-                .count(),
-        )
+        Some(field.count(field.start, 64, true))
     }
     fn part2(input: Vec<String>) -> Option<Solution> {
         let field = Self::parse(&input);
         let w = field.width;
+        let w2 = w / 2;
+        println!("{w} {w2}");
+        let all_steps = 26501365;
+        // let all_steps = 589;
+        let all_steps = (4 * w) + w2;
+        assert_eq!((all_steps - (w + w2)) % (w * 3), 0);
+        let diagonal_tile = field.count(field.start, w + w2, true);
+        println!("a tile has {diagonal_tile}");
+        let diagonal_tile_x9 = field.count(field.start, (4 * w) + w2, true);
+        println!("{all_steps} steps => {diagonal_tile_x9}");
+        let diagonal_width_2 = ((all_steps - (w + w2)) / (w * 3)) as u64;
+        let diagonal_width = (diagonal_width_2 * 2) + 1;
+        let diagonal_tile_count = diagonal_width * diagonal_width;
+        println!("{diagonal_width_2} {diagonal_width} => {diagonal_tile_count} tiles");
+        Some(diagonal_tile_count * diagonal_tile)
         // Some interesting notes:
         // There is an unobstructed vertical and horizontal corridor starting exactly in the
         // center. This means we don't have to worry about any obstructions when figuring out the
@@ -132,6 +155,5 @@ impl DaySolver<Solution> for Day21 {
         // It takes 130/2 = 65 steps to get to the edge from the middle, so 26501300 steps in every
         // cardinal direction to get to the tips of the diamond.
         //
-        None
     }
 }
